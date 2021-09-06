da98830ab80a7f7496af45f05b790682de4ce231
andre <pitymaia@gmail.com>
2021-09-05 21:33:50 -0300

    All task

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+	const value = document.getElementById("principal").value;
+	const rate = document.getElementById("rate").value;
+	const years = document.getElementById("years").value;
+	const result = document.getElementById("result");
+
+	const amount = rate ? parseInt(value) + parseInt(value * ((rate / 100) * parseInt(years))) : parseInt(value);
+
+	var yearFromNow = new Date();
+	yearFromNow.setFullYear(yearFromNow.getFullYear() + parseInt(years));
+
+	result. innerHTML = `<div>If you deposit <span class="highlight">${value}</span>,</div>
+	<div>at an interest rate of <span class="highlight">${rate}%</span>.</div>
+	<div>You will receive an amount of <span class="highlight">${amount}</span>,</div>
+	<div>in the year <span class="highlight">${yearFromNow.getFullYear()}</span></div>`;
 }
-        
+
+function changeRate() {
+	const rateInput = document.getElementById("rate");
+	const rateValue = document.getElementById("rate-value");
+	rateValue.innerText = rateInput.value + "%";
+}
